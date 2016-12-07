47021dec07b62e65e091d951c4057aaf9f8e7329
darren-benson-mscst <hw3893dl@my.southeastmn.edu>
2016-12-06 18:15:18 -0600

    Commit Exercise 2E

---
+++ dbenson2747ex2e/Account/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="dbenson2747ex2e.Global" Language="C#" %>
---
+++ dbenson2747ex2e/Account/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(dbenson2747ex2e.Startup))]
+namespace dbenson2747ex2e
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
